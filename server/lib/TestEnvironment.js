4a9dc3b28b50a5648aca46a283f90baf03cc32de
mbelrose <mike@belrose.net>
2023-06-15 10:19:00 -0500

    was not checking for android. also cleanup code

--- server/lib/TestEnvironment.js
+++ server/lib/TestEnvironment.js
@@ -1,23 +1,19 @@
 import os from 'os';
 
+const platform = os.platform();
+
 export const isDesktopLinux = () => {
-  const platform = os.platform();
   if (platform === 'linux') {
-    const desktopSession = process.env.XDG_CURRENT_DESKTOP;
-    if (desktopSession) {
-      return true;
-    }
+    return true;
+  } else {
+    return false;
   }
-  return false;
 };
 
 export const isAndroidLinux = () => {
-  const platform = os.platform();
-  if (platform === 'linux') {
-    const release = os.release();
-    if (release.includes('Android')) {
-      return true;
-    }
+  if (platform === 'android') {
+    return true;
+  } else {
+    return false;
   }
-  return false;
 };
