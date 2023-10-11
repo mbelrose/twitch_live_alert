91ab23fcfc6ce7f48f81e19c23aa446e8c6550b7
mbelrose <mike@belrose.net>
2023-10-11 14:21:15 -0500

    typo

--- server/TwitchLiveAlert.js
+++ server/TwitchLiveAlert.js
@@ -95,8 +95,8 @@ async function getStreamerInfo(ids, clientId, accessToken) {
     response => {
       if (response.ok) {
         response.json()
-        .then( repsonseData => {
-          streams = repsonseData.data;
+        .then( responseData => {
+          streams = responseData.data;
         }).catch(e=>console.log(e.message));
       } else {
         console.log(`The server returned an error.  ${response.status}: ${response.statusText}`);
